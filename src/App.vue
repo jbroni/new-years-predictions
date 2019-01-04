abf9f33243bef43c0fd14e47bb9195e762bc5165
Jesper Broni Andersen <jba@ccieurope.com>
2019-01-04 14:00:35 +0100

    Load data from firestore

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,42 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <div v-for="question in questions">
+      {{question.question}} {{question.outcome}}
+    </div>
+    <div v-for="participant in participants">
+      {{participant.name}}
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
+import { firebaseConfig } from '../environments/firebase';
 import HelloWorld from './components/HelloWorld.vue';
 
+import Firebase from 'firebase';
+
+const firebaseApp = Firebase.initializeApp(firebaseConfig);
+const firestore = firebaseApp.firestore();
+const settings = {
+  timestampsInSnapshots: true
+};
+firestore.settings(settings);
+
 @Component({
+  data: () => {
+    return {
+      questions: [],
+      participants: [],
+    };
+  },
   components: {
     HelloWorld,
+  },
+  firestore: {
+    questions: firestore.collection('questions'),
+    participants: firestore.collection('participants'),
   },
 })
 export default class App extends Vue {}
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,10 @@
 import Vue from 'vue';
+import VueFire from 'vuefire';
 import App from './App.vue';
 
 Vue.config.productionTip = false;
+
+Vue.use(VueFire);
 
 new Vue({
   render: (h) => h(App),
