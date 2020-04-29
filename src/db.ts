2a91e6d003ed8e5d6b1127629d85f0a8565adea4
Jesper Broni Andersen <jba@stibodx.com>
2020-04-29 14:17:28 +0200

    Remove obsolete firebase setting
    > @firebase/firestore: Firestore (7.14.1): The setting 'timestampsInSnapshots: true' is no longer required and should be removed.

--- src/db.ts
+++ src/db.ts
@@ -2,7 +2,3 @@ import * as firebase from 'firebase';
 import { firebaseConfig } from '../environments/firebase';
 
 export const db = firebase.initializeApp(firebaseConfig).firestore();
-const settings = {
-  timestampsInSnapshots: true
-};
-db.settings(settings);
